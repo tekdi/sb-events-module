b977f6db00ed5b22d4845f660419af7fb92be1f1
Pratiksha khandagale <pratiksha_k@techjoomla..com>
2021-03-15 22:56:28 +0530

    Task #169595 feat: Event time show all over world

--- projects/event-library/src/lib/events/components/cover-event-detail/cover-event-detail.component.ts
+++ projects/event-library/src/lib/events/components/cover-event-detail/cover-event-detail.component.ts
@@ -7,10 +7,27 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CoverEventDetailComponent implements OnInit {
   @Input() eventDetailItem: any;
-  isEnrolled: boolean = true;
-  constructor() { }
+  @Input() userData: string;
+  eStart: any;
+  eEnd: any;
+  isEnrolled: boolean = false;
+  constructor(
+    private eventService: EventService,
+    private timezoneCal: TimezoneCal) {
+  }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.setDateTimeOnCover();
+    }, 1000);
+  }
+
+  /**
+   * for show Date Time as per timezone
+   */
+  setDateTimeOnCover() {
+    this.eStart = (this.timezoneCal.calcTime(this.eventDetailItem.startDate, this.eventDetailItem.startTime)).toLocaleString();
+    this.eEnd = (this.timezoneCal.calcTime(this.eventDetailItem.endDate, this.eventDetailItem.endTime)).toLocaleString();
   }
 
   isEnrollEvent(){
