bcbb274ac91970028a4688cc34e67406410c46e1
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2021-04-09 12:50:45 +0530

    Task #169667 feat : Join button hide/show and added enroll button

--- projects/event-library/src/lib/events/components/join-event-button/join-event-button.component.ts
+++ projects/event-library/src/lib/events/components/join-event-button/join-event-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { TimezoneCal } from '../../services/timezone/timezone.service';
+import { EventService } from '../../services/event/event.service'
 
 @Component({
   selector: 'sb-join-event-button',
@@ -9,34 +10,90 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class JoinEventComponent implements OnInit {
   @Input() eventDetailItem: any;
+  @Input() userData: string;
+  @Input() canUnenroll: boolean = true;
+
   todayDateTime: any;
   isUserAbleToJoin: boolean = false;
+  isEnrolled: boolean = false;
+  today: any;
+  todayDate: any;
+  todayTime: any;
+  startInMinutes: any;
+  items: any;
 
-  constructor() {
+  constructor(
+    private eventService: EventService,
+    private timezoneCal: TimezoneCal
+        ) {
   }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.isEnrollEvent();
+    }, 1000);
 
     setInterval(() => {
       this.joinEvent();
     }, 1000);
   }
 
-  joinEvent() {
-    this.todayDateTime = new Date();
+  /**
+   * For validate and show/hide join button
+   */
+  async joinEvent() {
 
-    var startEventTime = new Date(this.eventDetailItem.startDate + " " + this.eventDetailItem.startTime);
-    var startDifference = startEventTime.getTime() - this.todayDateTime.getTime();
+    this.today = new Date();
+    this.todayDate = this.today.getFullYear() + '-' + ('0' + (this.today.getMonth() + 1)).slice(-2) + '-' + ('0' + this.today.getDate()).slice(-2);
+    this.todayTime = this.today.getHours() + ":" + this.today.getMinutes();
+
+
+    var todayDateTime = this.timezoneCal.calcTime(this.todayDate, this.todayTime);
+    var startEventTime = await this.timezoneCal.calcTime(this.eventDetailItem.startDate, this.eventDetailItem.startTime);
+
+    var startDifference = startEventTime.getTime() - todayDateTime.getTime();
     var startInMinutes = Math.round(startDifference / 60000);
 
-    var endEventTime = new Date(this.eventDetailItem.endDate + " " + this.eventDetailItem.endTime);
-    var endDifference = this.todayDateTime.getTime() - endEventTime.getTime();
-    var endInMinutes = Math.round(endDifference / 60000);
+    var endEventTime = this.timezoneCal.calcTime(this.eventDetailItem.endDate, this.eventDetailItem.endTime);
 
+    var endDifference = todayDateTime.getTime() - endEventTime.getTime();
+    var endInMinutes = Math.round(endDifference / 60000);
     this.isUserAbleToJoin = (startInMinutes <= 10 && endInMinutes < 0) ? true : false;
 
   }
 
+  /**
+    * For check user is enrolled or not
+    * @param courseId Event id
+    * @param userId Log-in user Id 
+    */
+  isEnrollEvent() {
+    this.eventService.getEnrollEvents(this.eventDetailItem.identifier, this.userData).subscribe((data) => {
+      this.items = data.result.courses;
+
+      this.items.find((o, i) => {
+        if (o.courseId === this.eventDetailItem.identifier) {
+          this.isEnrolled = true;
+        }
+
+      });
+    });
+  }
+
+    /**
+   * Enroll/Unenroll event
+   * 
+   * @param action enroll/unenroll 
+   */
+     enrollToEvent(action) {
+      this.eventService.enrollToEventPost(action, this.eventDetailItem.code, this.userData);
+    }
+
+  /**
+   * For join attain event
+   * 
+   * @param joinLink event join url
+   */
   openProviderLink(joinLink) {
     window.open(joinLink, "_blank");
   }
